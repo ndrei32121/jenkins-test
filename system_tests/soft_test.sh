8464ff84586eacfebb89bc842f7addc227cb6df2
Andrei Lazarev <and.blkv@gmail.com>
2022-01-26 23:05:17 +0300

    soft checker script init

---
+++ system_tests/soft_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+APP_ARG=$1
+
+function soft_checker {
+    APP=$1
+    which $APP 2>&1 > /dev/null
+    RSLT=$?
+    if [ $RSLT -ne 0 ]
+    then
+        echo "$APP: not installed"
+        exit 1
+    else
+        echo "$APP: OK"
+    fi
+}
+
+soft_checker "$APP_ARG"
